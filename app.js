3090c48bd327ec38bec04e2360aea3420c0c6bda
Carolina Danae Rodriguez Jimenez <c_danae@hotmail.com>
2022-09-30 17:27:40 -0500

    Experimento de carrusel con JS parte 1

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* MENÚ DESPLEGABLE VERSIÓN MOBILE */
 let menu = document.getElementById("menu")
 let nav = document.getElementById("nav")
 let close = document.getElementById("close")
@@ -10,3 +11,25 @@ close.addEventListener("click", function (){
     nav.classList.remove("menu_activate")
 });
 
+/* CARRUSEL VERSIÓN MOBILE */
+const grande = document.querySelector(".grande")
+const spot = document.querySelectorAll(".spot")
+
+//Cuando CLICK en punto
+    //Saber la posición de ese spot
+    //Aplicar un transform translateX al grande
+    //QUITAR la clase activate de TODOS los spot
+    //AÑADIR la clase activate al spot que hemos hecho CLICK
+
+spot.forEach((cadaSpot , i)=> {
+    spot[i].addEventListener("click", () => {
+
+        let posicion = i
+        let operacion = posicion * -20
+        //posicion en 0 el transformX es 0
+        //posicion en 0 el transformX es -20%
+        //operacion = posicion * -20
+
+        grande.style.transform = `translateX(${operacion}%)`
+    })
+});
