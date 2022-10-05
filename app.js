6c4bb0f017dd6161dc2431b0b51e4dda7b71a42e
Carolina Danae Rodriguez Jimenez <c_danae@hotmail.com>
2022-10-04 21:39:10 -0500

    Benefits maquetacion terminada, falta responsive

--- app.js
+++ app.js
@@ -15,34 +15,26 @@ close.addEventListener("click", function (){
 const grande = document.querySelector(".carousel_container")
 const spot = document.querySelectorAll(".spot")
 
-//Cuando CLICK en punto
-    //Saber la posición de ese spot
-    //Aplicar un transform translateX al carousel_container
-    //QUITAR la clase activate de TODOS los spot
-    //AÑADIR la clase activate al spot que hemos hecho CLICK
 
-    //RECORRER TODOS los spot
 spot.forEach((cadaSpot , i)=> {
-   //ASIGNAMOS un CLICK a cada spot
     spot[i].addEventListener("click", () => {
-
-        //GUARDAR la posición de ese spot
         let position = i
-        //CALCULANDO el espacio que debe desplazarse el carousel_container
         let operation = position * -20
-        //position en 0 el transformX es 0
-        //position en 0 el transformX es -20%
-        //operation = posicion * -20
-
-        //MOVEMOS el carousel_container
+  
         grande.style.transform = `translateX(${operation}%)`
 
-        //RECORREMOS todos los spot
         spot.forEach( (cadaSpot, i) => {
-            //QUITAMOS el class activate a todos los spot
             spot[i].classList.remove("activate")
         })
-        //AÑADIR el class activate en el spot que se hace CLICK
+            
         spot[i].classList.add("activate")
     })
 });
+
+
+        
+
+
+        
+
+
